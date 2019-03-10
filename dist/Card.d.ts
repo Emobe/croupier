d6fad8a2ea77013943a938097236f297829b4ce6
Emobe <anthony.r.shuker@gmail.com>
2019-03-10 11:25:51 +0000

    add prettier + format project

--- dist/Card.d.ts
+++ dist/Card.d.ts
@@ -1,26 +1,26 @@
 export declare enum Suit {
-    Diamonds = "Diamonds",
-    Spades = "Spades",
-    Hearts = "Hearts",
-    Clubs = "Clubs"
+  Diamonds = 'Diamonds',
+  Spades = 'Spades',
+  Hearts = 'Hearts',
+  Clubs = 'Clubs'
 }
 export declare enum Rank {
-    Two = 2,
-    Three = 3,
-    Four = 4,
-    Five = 5,
-    Six = 6,
-    Seven = 7,
-    Eight = 8,
-    Nine = 9,
-    Ten = 10,
-    Jack = 11,
-    Queen = 12,
-    King = 13,
-    Ace = 14
+  Two = 2,
+  Three = 3,
+  Four = 4,
+  Five = 5,
+  Six = 6,
+  Seven = 7,
+  Eight = 8,
+  Nine = 9,
+  Ten = 10,
+  Jack = 11,
+  Queen = 12,
+  King = 13,
+  Ace = 14
 }
 export default class Card {
-    suit: Suit;
-    rank: Rank;
-    constructor(suit: Suit, rank: Rank);
+  suit: Suit;
+  rank: Rank;
+  constructor(suit: Suit, rank: Rank);
 }
