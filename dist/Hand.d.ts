ff37101e5022aa0041536c1f8f1b401d85b717a0
Anthony Shuker <anthony@emobe.uk>
2019-06-16 10:34:23 +0100

    Compile...

---
+++ dist/Hand.d.ts
@@ -0,0 +1,2 @@
+import { Card } from '.';
+export declare type Hand = Card[];
--- index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Deck } from './src/Deck';
-export { default as Card, Suit, Rank, iCard } from './src/Card';
