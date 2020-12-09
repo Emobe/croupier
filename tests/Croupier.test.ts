3731c12c0a1a2424ccd4d7ef7ec1a48f5c7bf54e
Anthony Shuker <anthony@emobe.uk>
2020-12-09 10:29:37 +0000

    update version

--- tests/Croupier.test.ts
+++ tests/Croupier.test.ts
@@ -1,11 +1,1 @@
-import Card from '../src/Card';
-import Croupier from '../src/Croupier';
-import test from 'tape';
-
-import sinon from 'sinon';
-
 // TODO spy on Deck
-test.skip('Croupier calls Deck constructor', t => {
-  t.plan(1);
-  const croupier = new Croupier();
-});
