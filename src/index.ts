16d57e2da0004ce78f09f59f6cf1bd6e9f6192f6
Anthony Shuker <anthony@emobe.uk>
2022-12-20 21:37:52 +0000

    Remove croupier file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import Croupier from './Croupier';
+//import Croupier from './Croupier';
 export { default as Deck } from './Deck';
 export * from './Hand';
 export { default as Card, Rank, Suit } from './Card';
-
-export default Croupier;
+//export default Croupier;
