1519b5943ea9495e5f315c0d2b50a984efc4068d
Anthony Shuker <anthony@emobe.uk>
2022-12-20 21:32:57 +0000

    Swap enums for iterable unions.

--- src/Deck.ts
+++ src/Deck.ts
@@ -1,4 +1,10 @@
-import { default as Card, Rank, Suit } from './Card';
+import { default as Card, Rank, Suit, ranks, suits } from './Card';
+
+interface DeckOptions {
+  seed?: number;
+  shuffle?: boolean;
+  jokers?: boolean;
+}
 
 export default class Deck {
   private cards: Card[] = [];
@@ -6,31 +12,34 @@ export default class Deck {
   /**
    * Create a deck of cards
    */
-  constructor() {
-    Object.keys(Suit).filter(suit => {
-      Object.keys(Rank).filter(rank => {
-        if (!isNaN(Number(rank))) {
-          this.cards.push(new Card(Suit[suit as keyof typeof Suit], Rank[rank as keyof typeof Rank]));
-        }
-      });
-    });
+  constructor(options: DeckOptions) {
+    // TODO add jokers
+    const { seed = Math.random(), shuffle = true } = options;
+    for (const suit of suits) {
+      for (const rank of ranks) {
+        this.cards.push(new Card(suit, rank));
+      }
+    }
+
+    if (shuffle) {
+      this.shuffle(seed);
+    }
   }
 
-  /**
-   * Returns an array of cards in the deck
-   */
   get Cards(): Card[] {
     return this.cards;
   }
-  /**
-   *
-   * @param amount Returns specified number of cards and takes them from the top of the deck
-   */
+
+  get Count(): number {
+    return this.cards.length;
+  }
+
   public take(amount: number): Card[] {
     return this.cards.splice(-Math.abs(amount), amount);
   }
 
   public shuffle(seed = Math.random()): void {
+    console.log('shuffling', seed);
     let currentIndex = this.cards.length;
     let temporaryValue;
     let randomIndex;
