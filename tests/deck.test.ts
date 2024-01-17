0f4601fa224961da77e3dda358b5f7efd3c4a389
Emobe <anthony.r.shuker@gmail.com>
2024-01-17 20:18:01 +0100

    Switch arrays for enums and create new tests

--- tests/deck.test.ts
+++ tests/deck.test.ts
@@ -1,28 +1,39 @@
 import { mockDeck, mockShuffledDeck } from './deck.mocks';
 import Deck from '../src/Deck';
+import { Rank } from '../src/Card';
 
 describe('Deck', () => {
-  it('should create a deck with no options passed', () => {
-    const deck = new Deck();
-    expect(deck).toBeTruthy();
+  it('should create a deck with 52 cards', () => {
+    const deck = new Deck({shuffle: false});
+    expect(deck.Count).toBe(52);
   });
-  it('should create a deck of unshuffled cards', () => {
+});
+
+// TODO make it check an actual seed
+describe('Deck Shuffle', () => {
+  it('should shuffle the deck of cards', () => {
     const deck = new Deck({ shuffle: false });
-    expect(deck.Cards).toEqual(mockDeck);
-  });
+    const firstCardPreShuffle = deck.Cards[0];
+    deck.shuffle();
+    const firstCardPostShuffle = deck.Cards[0];
 
-  it('should create a shuffled deck of cards with specified seed', () => {
-    const seed = 0.5;
-    const deck = new Deck({ seed });
-    expect(deck.Cards).toEqual(mockShuffledDeck);
+    expect(firstCardPreShuffle).not.toEqual(firstCardPostShuffle);
   });
+});
 
-  it('should be able to take X amount of cards from the deck', () => {
-    const seed = 0.5;
-    const deck = new Deck({ seed });
-    const countToTake = 5;
-    const hand = deck.take(countToTake);
-    expect(hand).toHaveLength(countToTake);
-    expect(deck.Count).toEqual(47);
+fdescribe('Deck Enum Tes', () => {
+    const deck = new Deck({ shuffle: false });
+    const firstCardPreShuffle = deck.Cards[0];
+    expect(firstCardPreShuffle.rank).toBe(Rank.Two);
+
+});
+
+describe('Take Cards from Deck', () => {
+  it('should take the specified number of cards from the deck', () => {
+    const deck = new Deck();
+    const cardsTaken = deck.take(5);
+    
+    expect(cardsTaken.length).toBe(5);
+    expect(deck.Count).toBe(47);
   });
 });
