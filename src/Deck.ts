c8f623db466120396402da4c42e594164974c2a2
Anthony Shuker <anthony@emobe.uk>
2026-02-19 07:51:31 +0000

    Adds ability for user to add a seed for predertermined suffling

--- src/Deck.ts
+++ src/Deck.ts
@@ -19,9 +19,9 @@ export class Deck {
     }
   }
 
-  shuffle(): void {
+  shuffle(seed?: number): void {
     for (let i = this.cards.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
+      const j = Math.floor(seed || Math.random() * (i + 1));
       [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
     }
   }
